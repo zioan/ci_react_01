ec20350e036a67ac8631eeacb8a68f4117fcca0d
Ioan Zaharia <zaharia.ioan@gmail.com>
2024-08-27 11:14:37 +0000

    Using a callback to run code the right way after updating state

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -10,18 +10,32 @@ class StatefulGreeting extends React.Component {
     };
   }
 
+  // Updating the state is asynchronous, the console logs will not show the updated state correctly
+  //since the state is updated in the background.
+  // To see the updated state, you can use the second argument of the setState method, which is a callback function.
+
+  // The main takeaway is that you should not rely on the state being updated immediately after calling setState.
+  // Instead, you should use the second argument of the setState method to run code after the state has been updated.
   handleClick() {
     if (this.state.introduction === "Hello") {
-      this.setState({
-        introduction: "Goodbye",
-        buttonText: "Enter",
-      });
+      this.setState(
+        {
+          introduction: "Goodbye",
+          buttonText: "Enter",
+        },
+        () => {
+          console.log("new state", this.state.introduction);
+          console.log("new state", this.state.buttonText);
+        }
+      );
     } else {
       this.setState({
         introduction: "Hello",
         buttonText: "Exit",
       });
     }
+    console.log(this.state.introduction);
+    console.log(this.state.buttonText);
   }
 
   render() {
