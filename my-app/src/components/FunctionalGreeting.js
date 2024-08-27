80eab808145dfd33a44b8f3f95140064388b2d00
Ioan Zaharia <zaharia.ioan@gmail.com>
2024-08-27 08:55:37 +0000

    Simple functional component

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function FunctionalGreeting() {
+//   return <h1Hello from react!</h1>;
+// }
+
+const FunctionalGreeting = () => <h1>Hello from react!</h1>;
+
+export default FunctionalGreeting;
